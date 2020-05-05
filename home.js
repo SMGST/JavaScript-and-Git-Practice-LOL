65e8f75650dd6441edb128a936782a89a6010eee
Tamim Sarker <SMGST@users.noreply.github.com>
2020-05-05 19:43:10 +0600

    commit 1

---
+++ home.js
@@ -0,0 +1,8 @@
+var AGE = 0;
+function set(){
+	AGE = prompt('What is your age ?');
+}
+function get() {
+	document.getElementById('demo').innerHTML = AGE;
+}
+console.log(50 / 5)
---
+++ myScript.js
@@ -0,0 +1,21 @@
+function hide(){
+			document.getElementById('hide').style.display='none';}
+function Show(){
+	document.getElementById('hide').style.display='block';}
+function setFontSize(n){
+	document.getElementById('demo').style.fontSize='35px';}
+function offLight(){
+	document.getElementById('image').src='pic_bulboff.gif';}
+function onLight(){
+	document.getElementById('image').src='pic_bulbon.gif';}
+function prints(){
+	document.write("Hello");}
+function myfun(){
+var x, y, z;  // Statement 1
+x = 5;        // Statement 2
+y = 6;        // Statement 3
+z = x + y;    // Statement 4
+
+document.getElementById("demo").innerHTML =
+"The value of z is " + z + "."; 
+}
